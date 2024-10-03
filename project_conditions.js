6161cf6aa5314f8d8da41f45409bd73ea35d683d
=Iyoha Louis <=louiscolix@gmail.com>
2024-10-03 07:59:01 +0100

    conditional statments

---
+++ project_conditions.js
@@ -0,0 +1,14 @@
+//the project was to create a prompt
+ //for the user to guess the right integer
+numbers = prompt("guess the number")
+if (number = 2) {
+     console.log("correct")
+} else {
+ console.log("wrong")    
+}
+
+
+
+ //i also tried the code in ternary conditions
+numbers = prompt("guess the number")
+ console.log(numbers=2 ? "correct" : "not correct") 
