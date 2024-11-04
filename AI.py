0816fa73dda3ebe76f90406376379f9de117facd
=Iyoha Louis <=louiscolix@gmail.com>
2024-11-04 04:27:04 +0100

    v3.0 includes more projects and solutions

--- AI.py
+++ AI.py
@@ -1,4 +1,4 @@
-# '''
+
 # def addtwo(i,j):
 #      i + j 
 
@@ -34,34 +34,80 @@
 #      sqr_root = sqrt(16)
 
 
-#      num2 = [2,3,10,5,12,10]
-# '''
-# index = 0
-# sum_of_num2 = 0
-# while index < len(num2):
-# 	sum_of_num2 = sum_of_num2 + num2[index]
-# 	index = index + 1
-
-# while 0 < 6:
-# 	sum_of_num2 = 0 + 2
-# 	index = 1
-
-# while 1 < 6:
-# 	sum_of_num2 = 2 + 3
-# 	index = 2
-
-# while 2 < 6:
-# 	sum_of_num2 = 5 + 10
-# 	index = 3
-
-# while 3 < 6:
-# 	sum_of_num2 = 15 + 5
-# 	index = 4
-# while 4 < 6:
-# 	sum_of_num2 = 20 + 12
-#    	index = 5
-# while 5 < 6:
-# 	sum_of_num2 = 32 + 10
-# 	index = 6
+#   = [2,3,10,5,12,10]
+# i = 0
+ #sum_of_num2 = 0
+ #while i < len(num2):
+ #	sum_of_num2 += num2[i]
+#	i = i + 1
 
 
+
+
+
+
+##write a program to increase the values in a list by 5
+
+#first_method
+#the_list = [2,4,6,8]
+#my_list = [x + 5 for x in the_list]
+
+#second_method
+#my_list = list()
+#for x in the_list:
+     #my_list.append(x+5)
+     #print("my_list: ", my_list)
+
+#name = "frank"
+
+
+
+
+
+#importing libraries
+# import pandas as pd
+# import NUMPY as np
+# import SEASON AS SNS
+# import matplotlib.pyplot as plt
+
+#reading the data
+#credit_risk = np.zeros
+
+
+
+d = {"mary":25,"hannah":25,"matthew":25}
+d = d["mary"]
+
+d = {"mary":[15,15,15], "hannah":[15,15,15], "matthew":[15,15,15]}     
+for k in d:
+     print(k)
+     for k,v in d.items():
+          print(k + "-" + str(v))
+
+          for score in d.values():
+               print(score)
+
+
+               output = []
+               for sscore in d.values():
+                    output.append(score[2])
+
+
+                    ###SET
+                    tuple
+                    
+
+
+
+                    ##DATA COLLECTION
+                    #DATA PREPARATION
+                    #MODEL BUILDING
+                    #MODEL TRAINING
+                    #MODEL PREDICTION
+                    #MODEL EVALUATION
+
+                    # 
+                    # 
+                    # 
+                    # ARRAY({1,2,3,4,5})
+                    # DATA3 = DATA3.RESHAPE (5,4)
